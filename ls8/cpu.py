ef1d90067617c12e2fd5107365735d2742bc4695
Bayron Puac <bayronpuac@gmail.com>
2020-03-25 15:10:48 -0500

    Working on day 2 and day 3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,39 +2,42 @@
 
 import sys
 
-ldi = 0b10000010
-prn = 0b01000111
-hlt = 0b00000001
-
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 8
+        self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+    
+    
+    def ram_read(self, mar):
+        return self.ram[mar]
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr    
 
     def load(self):
         """Load a program into memory."""
+        self.read_params()
 
-        address = 0
+        # address = 0
 
-        # For now, we've just hardcoded a program:
+        # # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -65,13 +68,15 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
-    def ram_read(self, mar):
-        return self.ram[mar]
-    def ram_write(self, mar, mdr):
-        self.ram[mar] = mdr    
 
     def run(self):
         """Run the CPU."""
+
+        ldi = 0b10000010
+        prn = 0b01000111
+        hlt = 0b00000001
+        mlt = 0b10100010
+
         running = True
         ram = self.ram 
         reg =  self.reg 
@@ -82,6 +87,8 @@ class CPU:
             if command == ldi:
                 reg_num = self.ram_read(pc + 1)
                 value = self.ram_read(pc + 2)
+                self.ram_write(reg_num,value)
+                pc += 3
             elif command == prn:
                 reg_num = self.ram_read(pc + 1)
                 print(reg[reg_num])
@@ -89,6 +96,12 @@ class CPU:
             elif command == hlt:
                 running = False 
                 pc += 1
+            elif command == mlt:
+                reg_num = self.ram_read(pc + 1)
+                reg_num2 = self.ram_read(pc + 2)
+                value = reg[reg_num] * reg[reg_num2]
+                reg[reg_num] = value
+
             else:
                 print(f"Unknown instruction: {command}")
                 
